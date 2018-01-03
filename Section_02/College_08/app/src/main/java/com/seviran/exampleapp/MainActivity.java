8b43e24e0aca77cddec894f2f545fabcbf383610
Seviran <schepens.arnout@gmail.com>
2018-01-03 15:06:21 +0100

    Textfields

--- Section_02/College_08/app/src/main/java/com/seviran/exampleapp/MainActivity.java
+++ Section_02/College_08/app/src/main/java/com/seviran/exampleapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +17,11 @@ public class MainActivity extends AppCompatActivity {
 
     public void clickFunction(View view)
     {
-        Log.i("Info", "Button pressed!");
+        EditText nameEditText = findViewById(R.id.nameEditText);
+        EditText passwordEditText = findViewById(R.id.passwordEditText);
+        Log.i("Info", "It worked!");
+        Log.i("Username", nameEditText.getText().toString());
+        Log.i("Password", passwordEditText.getText().toString());
     }
 
 }
