ec27d7ad68819d0d6ec1973fccf9f4cd94595998
Seviran <schepens.arnout@gmail.com>
2018-01-03 16:27:01 +0100

    Images

--- Section_02/College_08/app/src/main/java/com/seviran/exampleapp/MainActivity.java
+++ Section_02/College_08/app/src/main/java/com/seviran/exampleapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,7 +24,21 @@ public class MainActivity extends AppCompatActivity {
         Log.i("Info", "It worked!");
         Log.i("Username", nameEditText.getText().toString());
         Log.i("Password", passwordEditText.getText().toString());
-        Toast.makeText(this, "Hello " + nameEditText.getText().toString(), Toast.LENGTH_SHORT).show();
+        Log.i("Username", nameEditText.getText().toString());
+        if(nameEditText.getText().toString().isEmpty())
+        {
+            Toast.makeText(this, "Please enter your name", Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Toast.makeText(this, "Hello " + nameEditText.getText().toString(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    public void switchCat(View view)
+    {
+        ImageView catImageView = findViewById(R.id.catImageView);
+        catImageView.setImageResource(R.drawable.cat2);
     }
 
 }
